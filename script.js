a84a886fab86186c327fbc1e452daed8f51af5e6
sdimit <sdimit01@tufts.edu>
2014-01-03 02:57:10 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+$("document").ready(function(){
+
+//    $("td").mouseover(function(){
+//        $(this).attr("style","width: 50px; height: 50px; background-color: yellow;");                   
+//       
+     });
+    
+     $("td").mouseout(function(){
+//        $(this).attr("style","width: 25px; height: 25px; background-color: white;");    
+//    $(this).parent().attr("style","width: 25px; height: 50px; background-color: white;");                   
+               
+     });
+     
+     $("tbody").mouseout(function(){
+//     $("tr").attr("style","width: 25px; height: 25px; background-color: white;");                   
+                
+      });
+     
+    
+});
