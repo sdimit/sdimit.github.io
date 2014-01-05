f67f95608290718db670e4b620d643c5cd65ca36
sdimit <sdimit01@tufts.edu>
2014-01-05 11:08:14 +0200

    new thingy

--- script.js
+++ script.js
@@ -4,15 +4,26 @@ $("document").ready(function(){
 //    $("td").mouseover(function(){
 //        $(this).attr("style","width: 50px; height: 50px; background-color: yellow;");                   
 //       
-     });
+//    });
+
+  //  $(".icon").css("height", "100px");
     
-     $("td").mouseout(function(){
-//        $(this).attr("style","width: 25px; height: 25px; background-color: white;");    
+     $(".iconCenter").mouseover(function(){
+     $(".icon").toggleClass("hovered");
+     //         $(".icon").css("margin", "12.5px");
+     
+    //    $(".square").attr("style","margin: auto; width: 125px; height: 125; background-color:blue");    
 //    $(this).parent().attr("style","width: 25px; height: 50px; background-color: white;");                   
                
      });
      
-     $("tbody").mouseout(function(){
+     $(".iconCenter").mouseout(function(){
+               $(".icon").toggleClass("hovered");
+     
+       //   $(".icon").toggleClass("icon");
+     
+ //        $(".icon").css("margin", "0");
+     
 //     $("tr").attr("style","width: 25px; height: 25px; background-color: white;");                   
                 
       });
