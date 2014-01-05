91f3211c03021864dba92027ca41452decd8319e
sdimit <sdimit01@tufts.edu>
2014-01-06 01:56:43 +0200

    changed the animation a little

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ $("document").ready(function(){
     $(".icon").mouseover(function(){
         $("p")[1].style.color = "#35363a";
         $("p")[1].textContent = $(this).text();
+        $(".icon").css("background-size", "90%");
     });
     $(".icon").mouseout(function(){
         $("p")[1].style.color = "yellow";
+        $(".icon").css("background-size", "127%");
    //     $("p")[1].textContent = "";
     });
 });
