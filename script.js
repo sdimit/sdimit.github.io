51260d5c5a9cd1ffa918b3f39e9a2f39ed9ed125
sdimit <sdimit01@tufts.edu>
2014-01-06 00:49:09 +0200

    animated tooltips

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 $("document").ready(function(){
     $(".icon").mouseover(function(){
+        $("p")[1].style.color = "#35363a";
         $("p")[1].textContent = $(this).text();
     });
     $(".icon").mouseout(function(){
-        $("p")[1].textContent = "";
+        $("p")[1].style.color = "yellow";
+   //     $("p")[1].textContent = "";
     });
 });
